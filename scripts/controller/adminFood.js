a597a58f80b25e8ae6b0303398df854486e938d6
ngodonghong@gmail.com <ngodonghong@gmail.com>
2017-03-04 10:33:28 +0700

    Finish View and Delete Product

--- scripts/controller/adminFood.js
+++ scripts/controller/adminFood.js
@@ -1,9 +1,26 @@
 angular.module("minisnacks-admin").controller('AdminFood', ['$scope', '$http', function($scope, $http) {
 		$scope.foods = [];
+		$scope.pages = [];
+		productPerPage = 5;
 		$http.get("http://localhost:3000/foods").then(function (response) {
 			$scope.foods = response.data;
-			response.data.each(function(cur,index){
-				$scope.foods[index].no =index + 1;
-			});
+			for (i = 1; i <= Math.ceil($scope.foods.length/productPerPage); i++) {
+				$scope.pages.push(i);
+			}
 		});
+
+		$scope.goToPage = function(pageNum) {
+			$http.get("http://localhost:3000/foods?_page=" + pageNum + "&_limit=5").then(function (response) {
+				$scope.foods = response.data;
+			});
+		};
+
+		$scope.remove = function(id) {
+			for(i = 0; i < $scope.foods.length; i++) {
+				if ( $scope.foods[i].id == id ) {
+					$scope.foods.splice(i, 1);
+				}
+			}
+			$http.delete("http://localhost:3000/foods/" + id).then();
+		};
 	}]);
