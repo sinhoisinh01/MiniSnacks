e564fd5b1f1d51a03ea99e2f687d292383e2cd54
sinhoisinh01 <doanphucsinh@gmail.com>
2017-03-11 04:59:40 +0700

    Finish Signin and Signup

--- scripts/controller/IndexController.js
+++ scripts/controller/IndexController.js
@@ -1,5 +1,5 @@
 angular.module("minisnacks")
-	.controller('IndexController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
+	.controller('IndexController', ['$rootScope', '$scope', '$http', '$routeParams', '$location', function($rootScope, $scope, $http, $routeParams, $location) {
 		$scope.foods = [];
 		$scope.newComment = {};
 		$http.get("http://localhost:3000/foods").then(function (response) {
@@ -20,6 +20,64 @@ angular.module("minisnacks")
 				$scope.dish.comments.push($scope.newComment);
 				$scope.newComment = {};
 				//$http.put("http://localhost:3000/foods/" + foodId, {$scope.dish}).then();
+				req = {
+				 "method":"PUT",
+				 "url": "http://localhost:3000/foods/" + foodId,
+				 "headers": {
+				   "Content-Type": "application/json"
+				 },
+				 "data":$scope.dish
+				};
+				$http(req).then( function(response) {
+					console.log(response);	
+				});
 			}
-		}
+		};
+
+		$scope.register = function() {
+			if ($scope.registration) {
+				$http.get("http://localhost:3000/users?email_like=" + $scope.registration.email)
+				.then(function (response) {
+					for (i = 0; i < response.data.length; i++) {
+						if (response.data[i].email == $scope.registration.email) {
+							alert("Email đã đăng ký");
+							return;
+						}
+					}
+					req = {
+					 "method":"POST",
+					 "url": "http://localhost:3000/users/",
+					 "headers": {
+					   "Content-Type": "application/json"
+					 },
+					 "data":$scope.registration
+					};
+					$http(req).then( function(response) {
+						$rootScope.currentUser = response.data[i];
+						console.log($rootScope.currentUser);
+						window.location.href = "http://localhost/minisnacks/";
+					});
+				});
+			}
+		};
+
+		$scope.login = function() {
+			console.log($scope.email);
+			console.log($scope.password);
+			if ($scope.email && $scope.password) {
+				$http.get("http://localhost:3000/users?email_like=" + $scope.email)
+				.then(function (response) {
+					for (i = 0; i < response.data.length; i++) {
+						if (response.data[i].email == $scope.email && response.data[i].password == $scope.password) {
+							$rootScope.currentUser = response.data[i];
+							alert("Chào " + $rootScope.currentUser.firstname + "!!! Bạn đã đăng nhập thành công!!!");
+							window.location.href = "http://localhost/minisnacks/";
+						}
+						else {
+							alert("Sai Email hoặc mật khẩu");
+						}
+					}
+				});
+			}
+		};
 	}]);
