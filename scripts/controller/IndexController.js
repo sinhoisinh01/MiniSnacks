0b3dc924fc4e3b9344f2cf05868ae1f813b5caca
ngodonghong@gmail.com <ngodonghong@gmail.com>
2017-03-10 08:22:08 +0700

    View By Categories Ver.2

--- scripts/controller/IndexController.js
+++ scripts/controller/IndexController.js
@@ -1,9 +1,17 @@
 angular.module("minisnacks")
 	.controller('IndexController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
 		$scope.foods = [];
+		$scope.categories=[];
 		$scope.newComment = {};
+		
 		$http.get("http://localhost:3000/foods").then(function (response) {
 			$scope.foods = response.data;
+[]
+			$scope.dishcate = response.data;
+		});
+
+		$http.get("http://localhost:3000/categories").then(function (response) {
+			$scope.categories = response.data;
 
 		});
 
@@ -13,7 +21,25 @@ angular.module("minisnacks")
 			$http.get("http://localhost:3000/foods/" + foodId).then(function (response) {
 				$scope.dish = response.data;
 			});
-		}
+		}	
+
+		
+		categoryId = $routeParams.categoryId;
+
+		if (categoryId) {			
+
+			$http.get("http://localhost:3000/foods").then(function (response) {
+				$scope.dishcate = [];
+					for (var i = 0; i < response.data.length; i++) {
+						if(response.data[i].category.id == categoryId){
+							$scope.dishcate.push(response.data[i]);					 	
+					}					
+
+				}	
+			});
+		}	
+		
+			
 
 		$scope.AddNewComment = function() {
 			if ($scope.newComment.author && $scope.newComment.content) {
@@ -22,4 +48,5 @@ angular.module("minisnacks")
 				//$http.put("http://localhost:3000/foods/" + foodId, {$scope.dish}).then();
 			}
 		}
+		
 	}]);
--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,16 @@ app.config(function($locationProvider,$routeProvider) {
         templateUrl : "view/index.html",
 		      controller: "IndexController"
     })
+    .when("/shop/:categoryId", {
+        templateUrl : "view/shop.html",
+      	controller: "IndexController"
+    })
+
+    .when("/shop", {
+        templateUrl : "view/shop.html",
+      	controller: "IndexController"
+    })
+
 	.when("/details/:foodId", {
         templateUrl : "view/single.html",
 		      controller: "IndexController"
